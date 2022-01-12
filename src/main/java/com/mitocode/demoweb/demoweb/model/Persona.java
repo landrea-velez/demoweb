37cd1ebab06ea04c3d2df8071519c6b1672ff9e9
landrea-velez <landrea.velez@gmail.com>
2022-01-11 19:28:10 -0500

    first commit

---
+++ src/main/java/com/mitocode/demoweb/demoweb/model/Persona.java
@@ -0,0 +1,30 @@
+package com.mitocode.demoweb.demoweb.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Column;
+
+@Entity
+public class Persona {
+    @Id
+    private int idPersona;
+    @Column(name = "")
+    private String nombre;
+
+    public int getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(int idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+}
---
+++ src/test/java/com/mitocode/demoweb/demoweb/DemowebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mitocode.demoweb.demoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemowebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
